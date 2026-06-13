d02f02a83b2cd135fdc71852a325aaf55e21cf32
Fictophilians <adisetionugroho28@gmail.com>
2026-06-13 17:53:19 +0700

    pertama

--- script/product1.js
+++ script/product1.js
@@ -21,7 +21,7 @@ bg-white/20 outline-3 outline-gray-300">
       ${product.kategori}
     </p>
 
-    <a href="../view/login.html" class="font-bold mt-2 block hover:underline">
+    <a href="../view/detail.html?id=${product.id}" class="font-bold mt-2 block hover:underline">
       ${product.nama_barang}
     </a>
 
